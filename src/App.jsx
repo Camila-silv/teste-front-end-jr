b136570cff705ee4a8a32c0ea4066fd0757f98cf
Camila <cf.silv466@gmail.com>
2023-04-11 22:05:11 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState,  Fragment} from 'react';
+import MainHeader from "./components/main-header/main-header.jsx";
+import MainContent from "./components/main-content/main-content.jsx";
+import MainFooter from "./components/main-footer/main-footer.jsx";
+import Popup from "./components/popup/container-popup.jsx";
+
+export default function App() {
+  return (
+    <Fragment>
+      <Popup />
+      <MainHeader />
+      <MainContent />
+      <MainFooter />
+    </Fragment>
+  )
+}
---
+++ src/components/main-footer/miscellaneous-information/miscellaneous-information.jsx
@@ -0,0 +1,40 @@
+export default function MiscellaneousInformation() {
+  return (
+    <div className="about-the-company__miscellaneous-information">
+      <h3 className="miscellaneous-information__title">Informações úteis</h3>
+
+      <ul className="miscellaneous-information__list-miscellaneous-information">
+        <li>
+          <a className="list-miscellaneous-information__items" href="/">
+            Fale conosco
+          </a>
+        </li>
+        <li>
+          <a className="list-miscellaneous-information__items" href="/">
+            Dúvidas
+          </a>
+        </li>
+        <li>
+          <a className="list-miscellaneous-information__items" href="/">
+            Prazos de entrega
+          </a>
+        </li>
+        <li>
+          <a className="list-miscellaneous-information__items" href="/">
+            Formas de pagamento
+          </a>
+        </li>
+        <li>
+          <a className="list-miscellaneous-information__items" href="/">
+            Política de privacidade
+          </a>
+        </li>
+        <li>
+          <a className="list-miscellaneous-information__items" href="/">
+            Trocas e devoluções
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
